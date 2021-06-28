81c6d28d3f0474586846cb6e3a7a850ba1fff492
JcdeA <jcde@jcde.xyz>
2021-06-28 12:15:25 +0900

    feat: actually kinda works

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use hyper::service::{make_service_fn, service_fn};
 mod ssl;
 use core::task::{Context, Poll};
 use futures_util::stream::Stream;
-use hyper::{client, header::HeaderValue, Body, Request, Response, Server};
-
+use http::HeaderValue;
+use hyper::{client, Body, Request, Response, Server};
 use std::pin::Pin;
 use std::sync::Arc;
 use std::{io, sync};
@@ -17,9 +17,11 @@ type GenericError = Box<dyn std::error::Error + Send + Sync>;
 fn main() {
   // Serve an echo service over HTTPS, with proper error handling.
   if let Err(e) = run_server() {
-    eprintln!("FAILED: {}", e);
-    std::process::exit(1);
+    println!("FAILED: {}", e);
   }
+}
+fn error(err: String) -> io::Error {
+  io::Error::new(io::ErrorKind::Other, err)
 }
 
 #[tokio::main]
@@ -29,7 +31,6 @@ async fn run_server() -> Result<(), GenericError> {
     // Build an HTTP connector which supports HTTPS too.
     let mut http = client::HttpConnector::new();
     http.enforce_http(false);
-    // Build a TLS client, using the custom CA store for lookups.
     let tls = rustls::ClientConfig::new();
 
     // Join the above part into an HTTPS connector.
@@ -69,15 +70,27 @@ async fn run_server() -> Result<(), GenericError> {
   };
   let client: client::Client<_, hyper::Body> = hyper::client::Client::builder().build(https);
 
-  let new_service = make_service_fn(
-    |s: &tokio_rustls::server::TlsStream<tokio::net::TcpStream>| {
-      let (tcp_stream, inner) = s.into_inner();
-      let sni_hostname = inner.get_sni_hostname();
+  let service = make_service_fn(
+    move |s: &tokio_rustls::server::TlsStream<tokio::net::TcpStream>| {
       let client = client.clone();
-      async {
-        Ok::<_, GenericError>(service_fn(move |req: Request<Body>| {
-          // Clone again to ensure that client outlives this closure.
-          proxy(req, client.to_owned(), tcp_stream.peer_addr().unwrap())
+      let (tls_stream, server_session) = s.get_ref();
+      let a = tls_stream.peer_addr().unwrap();
+      async move {
+        let asdf = a.clone();
+        Ok::<_, io::Error>(service_fn(move |req: Request<Body>| {
+          let (mut parts, body) = req.into_parts();
+          parts.headers.remove("host");
+          parts.headers.append(
+            "host",
+            http::HeaderValue::from_str(&format!("{}", parts.uri.authority().unwrap())).unwrap(),
+          );
+          parts.headers.remove("x-forwarded-for");
+          parts.headers.append(
+            "x-forwarded-for",
+            http::HeaderValue::from_str(&format!("{}", asdf)).unwrap(),
+          );
+          let request: Request<Body> = Request::from_parts(parts, body);
+          proxy(request, client.to_owned())
         }))
       }
     },
@@ -86,7 +99,7 @@ async fn run_server() -> Result<(), GenericError> {
   let server = Server::builder(HyperAcceptor {
     acceptor: Box::pin(incoming_tls_stream),
   })
-  .serve(new_service);
+  .serve(service);
   server.await?;
 
   Ok(())
@@ -110,10 +123,9 @@ impl hyper::server::accept::Accept for HyperAcceptor<'_> {
 async fn proxy(
   req: Request<Body>,
   client: hyper::Client<hyper_rustls::HttpsConnector<hyper::client::HttpConnector>>,
-  remote_addr: std::net::SocketAddr,
 ) -> Result<Response<Body>, GenericError> {
   // Prepare the HTTPS connector.
-  let out_addr = "https://cloudflare.com";
+  let out_addr = "https://jcde.xyz";
 
   let uri_string = format!(
     "{}{}",
@@ -126,13 +138,13 @@ async fn proxy(
   )
   .to_owned();
   let (mut parts, body) = req.into_parts();
-  parts.headers.remove("Host");
-  parts.headers.append(
-    "x-forwarded-for",
-    HeaderValue::from_str(&format!("{}", remote_addr)).unwrap(),
-  );
+  parts.headers.remove("host");
+  //   parts.headers.append(
+  // "x-forwarded-for",
+  //  HeaderValue::from_str(&format!("{}", remote_addr)).unwrap(),
+  // );
   let mut request: Request<Body> = Request::from_parts(parts, body);
   *request.uri_mut() = uri_string.parse().unwrap();
-  let forward_res = client.request(request).await?;
-  Ok(forward_res)
+  let forward_res = client.request(request);
+  Ok(forward_res.await.unwrap())
 }
